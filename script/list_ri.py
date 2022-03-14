839f26652c5ab4e02f1a1a3d0d9adad0362c75c0
Donghyuck Kang <hyuckang15@gmail.com>
2022-03-14 17:04:51 +0900

    rds end date 추가 및 UTC+9으로 변경

--- script/list_ri.py
+++ script/list_ri.py
@@ -1,6 +1,7 @@
 #-*-coding:utf-8-*-
 try:
     from operator import itemgetter
+    from datetime import timedelta
     import boto3
 except ModuleNotFoundError as Module_E:
     print(Module_E)
@@ -14,6 +15,7 @@ except:
 
 ec2 = ses.client('ec2')
 rds = ses.client('rds')
+utc_9 = timedelta(hours=9)
 
 list_ec2_ri = ec2.describe_reserved_instances(
     Filters = [
@@ -31,22 +33,28 @@ list_rds_ri = sorted(list_rds_ri['ReservedDBInstances'], key=itemgetter('StartTi
 
 print('# EC2 RI #')
 if list_ec2_ri:
-    print('{0:<15} | {1:<20} | {2:<25} | {3:<25} | {4:<10} | {5:<3}'
-        .format('InstanceType', 'ProductDescription', 'Start', 'End', 'Price', 'Count'))
+    print('{0:<15} | {1:<20} | {2:<19} | {3:<19} | {4:<10} | {5:<3}'
+        .format('InstanceType', 'ProductDescription', 'Start UTC+9:00', 'End UTC+9:00', 'Price', 'Count'))
 
     for ec2_ri in list_ec2_ri:
+        start_time = (ec2_ri['Start'] + utc_9).strftime("%Y-%m-%d %H:%M:%S")
+        end_time = (ec2_ri['End'] + utc_9).strftime("%Y-%m-%d %H:%M:%S")
+        
         print('{0:<15} | {1:<20} | {2} | {3} | {4:<10} | {5:<3}'
-            .format(ec2_ri['InstanceType'], ec2_ri['ProductDescription'], ec2_ri['Start'], ec2_ri['End'], ec2_ri['FixedPrice'], ec2_ri['InstanceCount']))
+            .format(ec2_ri['InstanceType'], ec2_ri['ProductDescription'], start_time, end_time, ec2_ri['FixedPrice'], ec2_ri['InstanceCount']))
 else:
     print("EC2 RI List is not exist")
 
 print('# RDS RI #')
 if list_rds_ri:
-    print('{0:<15} | {1:<20} | {2:<25} | {3:<10} | {4:<3}'
-        .format('InstanceType', 'ProductDescription', 'Start', 'Price', 'Count'))
+    print('{0:<15} | {1:<20} | {2:<19} | {3:<19} | {4:<10} | {5:<3}'
+        .format('InstanceType', 'ProductDescription', 'Start  UTC+9:00', 'End  UTC+9:00', 'Price', 'Count'))
 
     for rds_ri in list_rds_ri:
-        print('{0:<15} | {1:<20} | {2} | {3:<10} | {4:<3}'
-            .format(rds_ri['DBInstanceClass'], rds_ri['ProductDescription'], rds_ri['StartTime'], rds_ri['FixedPrice'], rds_ri['DBInstanceCount']))
+        start_time = (rds_ri['StartTime'] + utc_9).strftime("%Y-%m-%d %H:%M:%S")
+        end_time = (rds_ri['StartTime'] + utc_9 + timedelta(seconds = rds_ri['Duration'])).strftime("%Y-%m-%d %H:%M:%S")
+        
+        print('{0:<15} | {1:<20} | {2} | {3} | {4:<10} | {5:<3}'
+            .format(rds_ri['DBInstanceClass'], rds_ri['ProductDescription'], start_time, end_time, rds_ri['FixedPrice'], rds_ri['DBInstanceCount']))
 else:
     print("RDS RI LIST is not exist")
